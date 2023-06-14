a0a4d58169fe38f25fcffa6a750b047ad1c17c40
Abdan Syakuran <abdansyakuran@hotmail.com>
2023-06-14 18:20:37 +0700

    Update Fonts and Resize Submit Form

---
+++ app/theme.js
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Montserrat Alternates, sans-serif",
+    fontWeight: 600,
+  },
+});
+
+export default theme;
--- next.config.js
+++ next.config.js
@@ -2,3 +2,24 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+module.exports = {
+  // ...
+  // Other Next.js configurations
+  // ...
+  // Add the following configuration for serving static files
+  // in the development and production environment
+  async headers() {
+    return [
+      {
+        source: "/fonts/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
+      },
+    ];
+  },
+};
